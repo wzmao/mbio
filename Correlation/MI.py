09a33d7ab8c0073e7d728817c91b6d8b28f71fe1
Wenzhi Mao <wem22@pitt.edu>
2013-09-17 11:23:24 -0400

    Move SCA functions into MI.py

--- Correlation/MI.py
+++ Correlation/MI.py
@@ -4,7 +4,7 @@ It also provide a shuffling function to calculate the shuffled P-value.
 The shuffled P-value calculation is performced by C and MPI.
 '''
 __author__ = 'Wenzhi Mao'
-__all__ = ['CalcMI', 'CalcMIp', 'CalcOMES']
+__all__ = ['CalcMI', 'CalcMIp', 'CalcOMES', 'CalcSCA']
 
 
 def _Startup():
@@ -114,6 +114,110 @@ def CalcOMES(sequences):
             omes.append([])
         omes[-1].append(result[i])
     return omes
+
+def CalcSCA(sequence):
+    from math import fsum as sum
+    from math import log
+
+    def mean(x):
+        return 1.0*sum(x)/len(x)
+    reslist = [i for i in 'ACDEFGHIKLMNPQRSTVWY']
+    q = [.073, .025, .050, .061, .042, .072, .023, .053, .064, .089,
+         .023, .043, .052, .040, .052, .073, .056, .063, .013, .033]
+    p = []
+    phi = []
+    m = len(sequence)
+    l = len(sequence[0])
+    for i in range(l):
+        temp = [sequence[j][i] for j in range(m)]
+        f = [temp.count(j)*1.0/m for j in reslist]
+        p.append([])
+        phi.append([])
+        sumphif = 0
+        for j in range(len(q)):
+            if 0 < f[j] < 1:
+                phi[-1].append(abs(log(f[j]*(1-q[j])/(1-f[j])/q[j])))
+                p[-1].append(f[j]*phi[-1][-1])
+            else:
+                phi[-1].append(0)
+                p[-1].append(0)
+            sumphif += p[-1][-1]**2
+        sumphif = sumphif**.5
+        if sumphif == 0.0:
+            for j in range(len(p[-1])):
+                p[-1][j] = 0.0
+        else:
+            for j in range(len(p[-1])):
+                p[-1][j] /= sumphif
+    xp = []
+    for i in range(m):
+        xp.append([])
+        for j in range(l):
+            if sequence[i][j] in reslist:
+                xp[-1].append(p[j][reslist.index(sequence[i][
+                              j])]*phi[j][reslist.index(sequence[i][j])])
+            else:
+                xp[-1].append(0)
+    meanxp = [mean([xp[j][i] for j in range(m)]) for i in range(l)]
+    c = [[0 for j in range(l)]for i in range(l)]
+    for i in range(l):
+        for j in range(i, l):
+            c[i][j] = abs(sum([xp[k][i]*xp[k][
+                          j] for k in range(m)])*1.0/m-meanxp[i]*meanxp[j])
+            c[j][i] = c[i][j]
+    return c
+
+def CalcSCA1(sequence):
+    from math import fsum as sum
+    from math import log
+
+    def mean(x):
+        return 1.0*sum(x)/len(x)
+    reslist = [i for i in 'ACDEFGHIKLMNPQRSTVWY']
+    q = [.073, .025, .050, .061, .042, .072, .023, .053, .064, .089,
+         .023, .043, .052, .040, .052, .073, .056, .063, .013, .033]
+    p = []
+    phi = []
+    m = len(sequence)
+    l = len(sequence[0])
+    for i in range(l):
+        temp = [sequence[j][i] for j in range(m)]
+        f = [temp.count(j)*1.0/m for j in reslist]
+        p.append([])
+        phi.append([])
+        sumphif = 0
+        for j in range(len(q)):
+            if 0 < f[j] < 1:
+                phi[-1].append(abs(log(f[j]*(1-q[j])/(1-f[j])/q[j])))
+                p[-1].append(f[j]*phi[-1][-1])
+            else:
+                phi[-1].append(0)
+                p[-1].append(0)
+            sumphif += p[-1][-1]**2
+        sumphif = sumphif**.5
+        if sumphif == 0.0:
+            for j in range(len(p[-1])):
+                p[-1][j] = 0.0
+        else:
+            for j in range(len(p[-1])):
+                p[-1][j] /= sumphif
+    xp = []
+    for i in range(m):
+        xp.append([])
+        for j in range(l):
+            if sequence[i][j] in reslist:
+                xp[-1].append(p[j][reslist.index(sequence[i][
+                              j])]*phi[j][reslist.index(sequence[i][j])])
+            else:
+                xp[-1].append(0)
+    meanxp = [mean([xp[j][i] for j in range(m)]) for i in range(l)]
+    c = [[0 for j in range(l)]for i in range(l)]
+    for i in range(l):
+        for j in range(i, l):
+            c[i][j] = abs(sum([xp[k][i]*xp[k][
+                          j] for k in range(m)])*1.0/m-meanxp[i]*meanxp[j])
+            c[j][i] = c[i][j]
+    return c,xp
 
 
 _Startup()
