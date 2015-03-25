afbcda104f9903bda2d82e34a6fdc63b6e2b52a9
Wenzhi Mao <maowenzhi@yandex.com>
2015-03-25 23:17:28 +0800

    Remove the cluster and job_organization.

--- mbio/Application/__init__.py
+++ mbio/Application/__init__.py
@@ -18,13 +18,13 @@ from . import sort
 from .sort import *
 __all__.extend(sort.__all__)
 
-from . import cluster
+# from . import cluster
 # from .cluster import *
 # __all__.extend(cluster.__all__)
 
-from . import job_organization
-from .job_organization import *
-__all__.extend(job_organization.__all__)
+# from . import job_organization
+# from .job_organization import *
+# __all__.extend(job_organization.__all__)
 
 from . import math
 # from .math import *
