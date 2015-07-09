d20ee32c952cd25684f67ec8d9ddcd18ec15747d
Wenzhi Mao <maowenzhi@yandex.com>
2015-07-09 11:58:01 +0800

    Add PDB IO which could parse Beta data.

--- mbio/IO/functions.py
+++ mbio/IO/functions.py
@@ -4,7 +4,8 @@
 
 __author__ = 'Wenzhi Mao'
 
-__all__ = ["parseMRC", "parseMRCHeader", "writeMRC"]
+__all__ = ["parseMRC", "parseMRCHeader", "writeMRC", "parsePDB",
+           "writePDB"]
 
 
 def parseMRC(filename=None, **kwargs):
@@ -26,6 +27,7 @@ def parseMRC(filename=None, **kwargs):
 
 def parseMRCHeader(filename=None, **kwargs):
     """Parse the MRC header from a file."""
+
     from .mrc import MRCHeader
 
     from os.path import exists, isfile
@@ -43,9 +45,45 @@ def parseMRCHeader(filename=None, **kwargs):
 
 def writeMRC(filename, mrc, **kwargs):
     """Write the MRC file into a file."""
+
     from .mrc import MRC
+
     if not isinstance(mrc, MRC):
         from .output import printError
         printError("The mrc is not a mbio MRC instance.")
         return None
     mrc.writeData(filename=filename, **kwargs)
+
+
+def parsePDB(filename, **kwargs):
+    """Read PDB file. If there exists a beta file read it also."""
+
+    from prody import parsePDB as p
+    from os.path import exists, isfile
+
+    pdb = p(filename, **kwargs)
+    if exists(filename + ".beta") and isfile(filename + ".beta"):
+        try:
+            f = open(filename + ".beta", 'r')
+            beta = [float(i) for i in f]
+            f.close()
+            pdb.setBetas(beta)
+        except:
+            pass
+    return pdb
+
+
+def writePDB(filename, pdb, **kwargs):
+    """Write PDB file. Write the beta to beta also."""
+
+    from prody import writePDB as p
+    from os.path import exists, isfile
+
+    p(filename, pdb, **kwargs)
+    if (exists(filename + ".beta") and isfile(filename + ".beta")) or not exists(filename + '.beta'):
+        try:
+            f = open(filename + ".beta", 'w')
+            f.write('\n'.join([str(i) for i in pdb.getBetas()]))
+            f.close()
+        except:
+            pass
